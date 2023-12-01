e9ea4c68f47278d5c2fedd2af64e926eff79bef3
Biswajit Debnath <biswajit.basanti@gmail.com>
2023-12-01 16:37:40 +0530

    feat: :sparkles: routing done ✔

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import './style/app.scss'
+import {  Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from './components/home/Home'
+import Data from './components/data/Data'
+import Transaction from "./components/transaction/Transaction";
+import NoPage  from "./components/noPage/NoPage"
 
 function App() {
 
   return (
     <div className="app">
-      hellow world
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="data" element={<Data />} />
+          <Route path="transacrton" element={<Transaction />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
